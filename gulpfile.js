6365fdee631fdd7ff04533d5578e12dfe0c9ccc0
Max Franz <maxkfranz@gmail.com>
2016-09-29 12:48:32 -0400

    Add `gulp watch` with livereload #22

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,6 @@
 var gulp = require('gulp');
+var util = require('gulp-util');
+var notifier = require('node-notifier');
 var path = require('path');
 var replace = require('gulp-replace');
 var child_process = require('child_process');
@@ -10,32 +12,106 @@ var exec = require('child_process').exec;
 var runSequence = require('run-sequence');
 var prompt = require('gulp-prompt');
 var browserify = require('browserify');
+var watchify = require('watchify');
+var babelify = require('babelify');
 var buffer = require('vinyl-buffer');
 var source = require('vinyl-source-stream');
-var gutil = require('gulp-util');
 var notifier = require('node-notifier');
 var derequire = require('gulp-derequire');
+var server = require('http-server');
+var livereload = require('gulp-livereload');
+var pkg = require('./package.json');
 var version;
 
-var browserifyOpts = {
-  entries: './src/index.js',
-  debug: true,
-  standalone: 'cytoscape-cose-bilkent'
+var logError = function( err ){
+  notifier.notify({ title: pkg.name, message: 'Error: ' + err.message });
+  util.log( util.colors.red(err) );
 };
 
-var logError = function( err ){
-  notifier.notify({ title: 'cose-bilkent', message: 'Error: ' + err.message });
-  gutil.log( gutil.colors.red('Error in watch:'), gutil.colors.red(err) );
+var handleErr = function( err ){
+  logError( err );
+
+  if( this.emit ){
+    this.emit('end');
+  }
+};
+
+var getBrowserified = function( opts ){
+  opts = Object.assign({
+    debug: true,
+    cache: {},
+    packageCache: {},
+    fullPaths: true,
+    bundleExternal: true,
+    entries: [ './src' ]
+  }, opts );
+
+  return browserify( opts ).on( 'log', util.log );
+};
+
+var transform = function( b ){
+  return ( b
+    // can't use babel because cose-bilkent does just use pure functions in workers...
+    // .transform( babelify.configure({
+    //   presets: ['es2015'],
+    //   ignore: 'node_modules/**/*',
+    //   sourceMaps: 'inline'
+    // }) )
+  ) ;
+};
+
+var bundle = function( b ){
+  return ( b
+    .bundle()
+    .on( 'error', handleErr )
+    .pipe( source('cytoscape-cose-bilkent.js') )
+    .pipe( buffer() )
+  ) ;
 };
 
 gulp.task('build', function(){
-  return browserify( browserifyOpts )
-    .bundle()
-    .on( 'error', logError )
-    .pipe( source('cytoscape-cose-bilkent.js') )
-    .pipe( buffer() )
-    .pipe( derequire() )
-    .pipe( gulp.dest('.') )
+  return bundle( transform( getBrowserified() ) )
+    .pipe( gulp.dest('./') )
+  ;
+});
+
+gulp.task('watch', function(){
+  livereload.listen({
+    basePath: process.cwd()
+  });
+
+  server.createServer({
+    root: './',
+    cache: -1,
+    cors: true
+  }).listen( '9999', '0.0.0.0' );
+
+  util.log( util.colors.green('Demo hosted on local server at http://localhost:9999/demo.html') );
+
+  gulp.watch( ['./cytoscape-cose-bilkent.js'] )
+    .on('change', livereload.changed)
+  ;
+
+  var update = function(){
+    util.log( util.colors.white('JS rebuilding via watch...') );
+
+    bundle( b )
+      .pipe( gulp.dest('./') )
+      .on('finish', function(){
+        util.log( util.colors.green('JS rebuild finished via watch') );
+      })
+    ;
+  };
+
+  var b = getBrowserified();
+
+  transform( b );
+
+  b.plugin( watchify, { poll: true } );
+
+  b.on( 'update', update );
+
+  update();
 });
 
 gulp.task('default', ['build'], function( next ){
